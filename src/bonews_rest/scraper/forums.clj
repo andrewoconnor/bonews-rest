05d32fcd7f080a085f7ed261eef793b58f152dcb
Andrew O'Connor <calusari@Ragnarok.local>
2015-09-25 10:42:38 -0400

    refactored return statements for parsing thread tables

--- src/bonews_rest/scraper/forums.clj
+++ src/bonews_rest/scraper/forums.clj
@@ -17,8 +17,7 @@
 
 (defn get-thread-num-replies
   [col]
-  (let [replies (first (first (rest (first (rest (rest col))))))]
-  (Integer/parseInt replies)))
+  (Integer/parseInt (first (:content col))))
 
 (defn get-thread-last-update
   [col]
@@ -27,29 +26,29 @@
 (defn get-author-url
   [col]
   (let [title-node (html/select col [[:a (html/attr? :href)]])]
-  (:href (first (rest (first (rest (last title-node))))))))
+  (:href (:attrs (last title-node)))))
 
 (defn get-thread-author
   [col]
   (let [title-node (html/select col [[:a (html/attr? :href)]])]
-  (first (first (rest (last (last title-node)))))))
+  (first (:content (last title-node)))))
 
 (defn get-thread-title
   [col]
   (let [title-node (html/select col [:h4 [:a (html/attr? :href)]])]
-  (first (first (rest (first (rest (rest (first title-node)))))))))
+  (first (:content (first title-node)))))
 
 (defn get-thread-url
   [col]
   (let [title-node (html/select col [:h4 [:a (html/attr? :href)]])]
-  (:href (first (rest (first (rest (first title-node))))))))
+  (:href (:attrs (first title-node)))))
 
 (defn get-cols
   [row]
   (let [cols (rest (html/select row [:td]))]
     {:thread-url            (get-thread-url (first cols))
      :thread-title          (get-thread-title (first cols))
-     :thread-num-replies    (get-thread-num-replies (first (rest cols)))
+     :thread-num-replies    (get-thread-num-replies (fnext cols))
      :thread-last-update    (get-thread-last-update (last cols))
      :author                (get-thread-author (first cols))
      :author-url            (get-author-url (first cols))}))
