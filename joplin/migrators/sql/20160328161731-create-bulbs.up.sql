407e756bafb17bb6aab599aae473494c6189322d
Andrew O'Connor <andrewoconnor@outlook.com>
2016-04-29 21:23:22 -0400

    cache votes given and received counts on user

--- joplin/migrators/sql/20160328161731-create-bulbs.up.sql
+++ joplin/migrators/sql/20160328161731-create-bulbs.up.sql
@@ -26,57 +26,54 @@ DECLARE
   table_name text := quote_ident(TG_ARGV[0]);
   fk_name text := quote_ident(TG_ARGV[1]);
   fk_changed boolean := false;
+  vote_type text;
   counter_name text;
+  step integer;
+  reply_id integer;
+  reply_author integer;
   fk_value integer;
+  vote integer;
   record record;
 BEGIN
   IF TG_OP = 'UPDATE' OR TG_OP = 'INSERT' THEN
-    IF NEW.vote = 1 THEN
-      IF table_name = 'replies' THEN
-        counter_name = 'upvotes_count';
-      ELSEIF table_name = 'users' THEN
-        counter_name = 'upvotes_given_count';
-      END IF;
-    ELSEIF NEW.vote = -1 THEN
-      IF table_name = 'replies' THEN
-        counter_name = 'downvotes_count';
-      ELSEIF table_name = 'users' THEN
-        counter_name = 'downvotes_given_count';
-      END IF;
-    END IF;
+    record := NEW;
+    step = 1;
   ELSEIF TG_OP = 'DELETE' THEN
-    IF OLD.vote = 1 THEN
-      IF table_name = 'replies' THEN
-        counter_name = 'upvotes_count';
-      ELSEIF table_name = 'users' THEN
-        counter_name = 'upvotes_given_count';
-      END IF;
-    ELSEIF OLD.vote = -1 THEN
-      IF table_name = 'replies' THEN
-        counter_name = 'downvotes_count';
-      ELSEIF table_name = 'users' THEN
-        counter_name = 'downvotes_given_count';
-      END IF;
-    END IF;
+    record := OLD;
+    step = -1;
   END IF;
 
+  vote = record.vote;
+  reply_id = record.reply_id;
+
+  IF vote = 1 THEN
+    vote_type = 'upvotes_';
+  ELSEIF vote = -1 THEN
+    vote_type = 'downvotes_';
+  END IF;
+
+  IF table_name = 'users' THEN
+    counter_name = vote_type || 'given_';
+    SELECT user_id INTO reply_author FROM replies WHERE id = reply_id;
+  ELSE
+    counter_name = vote_type;
+  END IF;
+
+  counter_name = counter_name || 'count';
+
   IF TG_OP = 'UPDATE' THEN
-    record := NEW;
     EXECUTE 'SELECT ($1).' || fk_name || ' != ' || '($2).' || fk_name
     INTO fk_changed
     USING OLD, NEW;
   END IF;
 
-  IF TG_OP = 'DELETE' OR fk_changed THEN
-    record := OLD;
+  IF TG_OP = 'INSERT' OR TG_OP = 'DELETE' OR fk_changed THEN
     EXECUTE 'SELECT ($1).' || fk_name INTO fk_value USING record;
-    PERFORM increment_counter(table_name, counter_name, fk_value, -1);
-  END IF;
-
-  IF TG_OP = 'INSERT' OR fk_changed THEN
-    record := NEW;
-    EXECUTE 'SELECT ($1).' || fk_name INTO fk_value USING record;
-    PERFORM increment_counter(table_name, counter_name, fk_value, 1);
+    PERFORM increment_counter(table_name, counter_name, fk_value, step);
+    IF table_name = 'users' THEN
+      counter_name = vote_type || 'received_count';
+      PERFORM increment_counter(table_name, counter_name, reply_author, step);
+    END IF;
   END IF;
 
   RETURN record;
